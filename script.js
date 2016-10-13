7158d4e89eb49dd1597f2a927e6763fc14b250e4
‘Matthew <matthew.lindly@hotmail.com>
2016-10-13 15:02:38 -0600

    New design, works flawlessly

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 'use strict';
 
 function createPixels() {
-  var canvas = document.querySelector('#pixel-canvass');
-  for(var i = 0; i < 1620; i++) {
+  var canvas = document.querySelector('.pixel-canvas');
+  for(var i = 0; i < 1624; i++) {
     var pixelSquare = document.createElement('DIV');
     canvas.appendChild(pixelSquare);
-    console.log('hello');
 
     pixelSquare.style.display = 'inline-block';
+    pixelSquare.style.width = '15.4px';
     pixelSquare.style.float = 'left';
-    pixelSquare.style.width = '16px';
-    pixelSquare.style.height = '16px';
+    pixelSquare.style.height = '15.4px';
     pixelSquare.style.backgroundColor = 'white';
     pixelSquare.style.border = '1px #d3d3d3 solid';
   }
 
   console.log(canvas);
+}
+
+function clickedColor(object) {
+  var currentColorBox = document.getElementById('current-color');
+  var allColors = document.getElementsByClassName('color');
+
+  for(var i = 0; i < allColors.length; i++) {
+  console.log(allColors[i]);
+    if(object.target === allColors[i]) {
+      currentColorBox.style.backgroundColor = allColors[i].style.backgroundColor;
+      console.log(allColors[i] + ' : ' + object.target);
+    }
+  }
 }
 
 createPixels();
